6d74db44ce60d2ddba443c0578acc0395ace616b
Alex1Jin <1023547537@qq.com>
2020-10-19 11:15:13 +0800

    Add files via upload

---
+++ mymath/src/20209324main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "../include/head.h"
+int main(void)
+{
+ int a,b;
+ int i=0;
+ printf("enter two numbers:");
+ scanf("%d %d",&a,&b);
+ printf("add:%d\n",add(a,b));
+ printf("sub:%d\n",sub(a,b));
+ printf("mul:%d\n",mul(a,b));
+ printf("div:%lf\n",div(a,b));
+ for(;i<9324;i++){
+ 
+ } 
+ return 0;
+}
+
